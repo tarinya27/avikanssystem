b84959eff90a005e11962c5e9812a47e1fe96be7
Tarinya Perera <136464059+tarinya27@users.noreply.github.com>
2024-08-09 08:58:58 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/login.dart';
 
+
 void main() {
   runApp(MyApp());
 }
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SignInScreen() ,
+      home: SignInScreen(),
     );
   }
 }
